43335241e5568e2e5c96383925d9a7f216dd06ee
khanhnh179 <nguyenhk179@gmail.com>
2016-10-05 11:07:56 +0700

    ver 0.1

---
+++ src/Student.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#ifndef STUDENT_H
+#define STUDENT_H
+#include<string>
+
+	using namespace std;
+
+class Student
+	//Student Management Model
+{
+public:
+	int id;
+	string name;
+	string address;
+	string gender;
+	float math;
+	float chem;
+	float phys;
+	float sum = math + chem + phys;
+	int schoolid;
+	string filename;
+
+	void setS(int, string, string, string, float, float, float, int); // lưu sinh viên vào mảng sinh viên
+
+	void getS(); // in thông tin sinh viên ra màn hình
+
+	void saveS(string); // lưu thông tin sinh viên vào file
+
+	~Student();
+};
+#endif // !STUDENT_H
